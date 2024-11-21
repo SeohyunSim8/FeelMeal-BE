58a7ddc26a41581c04b1ed80c041010094ab86d4
SeohyunSim <angelsh0805@gachon.ac.kr>
2024-11-22 03:28:55 +0900

    좌표 통합

--- src/main/java/feelmeal/api/coordinate/service/GeocoderService.java
+++
@@ -1,7 +0,0 @@
-package feelmeal.api.coordinate.service;
-
-import feelmeal.api.coordinate.service.dto.PostCoordinateServiceDto;
-
-public interface GeocoderService {
-    public void convertAddressToCoordinate(PostCoordinateServiceDto dto);
-}
